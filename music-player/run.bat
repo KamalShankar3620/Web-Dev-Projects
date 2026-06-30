346c1f172480b7930b5836cd7d33be98d8462ad5
Kamal Shankar <125025201+KamalShankar3620@users.noreply.github.com>
2026-07-01 01:49:10 +0530

    Delete music-player directory

--- music-player/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ===================================================
-echo   Starting Antigravity Music Player...
-echo   Open http://127.0.0.1:5000/ in your browser if
-echo   it does not open automatically.
-echo ===================================================
-python app.py
-pause
